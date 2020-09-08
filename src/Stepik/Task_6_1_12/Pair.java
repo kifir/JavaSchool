2e94fccea98b6fbee33875f04b6f716acf39008d
kifir <endofkifir@gmail.com>
2020-09-08 21:29:27 +0300

    Task 6.1.12

--- src/Stepik/Task_6_1_12/Pair.java
+++ src/Stepik/Task_6_1_12/Pair.java
@@ -1,4 +1,37 @@
 package Stepik.Task_6_1_12;
 
-public class Pair {
+import java.util.Objects;
+import java.util.Optional;
+
+class Pair<T1,T2> {
+    private T1 first;
+    private T2 second;
+
+    public T1 getFirst(){
+        return first;
+    }
+
+    public T2 getSecond() {
+        return second;
+    }
+
+    public Pair(T1 first, T2 second){
+        this.first = first;
+        this.second = second;
+    }
+
+    public boolean equals(Pair p) {
+        if (this == p) return true;
+        if (p == null || getClass() != p.getClass()) return false;
+
+        return Objects.equals(first, p.first) && Objects.equals(second, p.second);
+    }
+
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
+
+    public static <T1,T2> Pair<T1,T2> of(T1 first, T2 second){
+        return new Pair<>(first, second);
+    }
 }
--- src/Stepik/Task_6_1_12/Task_6_1_12.java
+++ src/Stepik/Task_6_1_12/Task_6_1_12.java
@@ -1,4 +1,15 @@
 package Stepik.Task_6_1_12;
 
 public class Task_6_1_12 {
+    public static void main(String[] args){
+        System.out.println("Task_6_1_12");
+
+        Pair<Integer, String> pair = Pair.of(1, "hello");
+        Integer i = pair.getFirst(); // 1
+        String s = pair.getSecond(); // "hello"
+
+        Pair<Integer, String> pair2 = Pair.of(1, "hello");
+        boolean mustBeTrue = pair.equals(pair2); // true!
+        boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
+    }
 }
