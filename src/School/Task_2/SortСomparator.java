0c8ff8725262be7181cc185ca58f075949f4b6f7
kifir <endofkifir@gmail.com>
2020-09-09 21:48:51 +0300

    Task 2

---
+++ src/School/Task_2/SortСomparator.java
@@ -0,0 +1,15 @@
+package School.Task_2;
+
+import java.util.Comparator;
+
+class SortComparator implements Comparator<String> {
+    @Override
+    public int compare(String s1, String s2) {
+        if (s1.length() == s2.length()){
+            return s1.compareTo(s2);
+        }
+        else {
+            return s1.length() - s2.length();
+        }
+    }
+}
---
+++ src/School/Task_2/Task_2.java
@@ -0,0 +1,48 @@
+package School.Task_2;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+
+public class Task_2 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Task_2");
+
+        final String dir = System.getProperty("user.dir");
+        System.out.println("current dir = " + dir);
+
+        String inputFileName = "src/School/Task_2/input.txt";
+
+        InputStream input = new FileInputStream(inputFileName);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        String line;
+        HashSet<String> distinctWords = new HashSet<>();
+
+//        while ((line = reader.readLine()) != null){
+//            System.out.println(line);
+//            distinctWords.addAll(Arrays.asList(line.split(" ")));
+//        }
+//        System.out.println();
+//        distinctWords.forEach(System.out::println);
+
+        while ((line = reader.readLine()) != null){
+            distinctWords.addAll(Arrays.asList(line.split(" ")));
+        }
+
+        System.out.println("Количество различных слов в файле " + distinctWords.size());
+
+        ArrayList<String> listWords = new ArrayList<>(distinctWords);
+        System.out.println(">>> Исходное множество слов");
+        listWords.forEach(System.out::println);
+        listWords.sort(new SortComparator());
+        System.out.println();
+        System.out.println(">>> Отсортированное множество слов");
+        listWords.forEach(System.out::println);
+
+    }
+
+
+}
+
