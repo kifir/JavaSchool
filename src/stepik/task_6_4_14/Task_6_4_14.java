fb90cafa0a8d8595ef2bcf5b181b95f75860c6a3
kifir <endofkifir@gmail.com>
2020-10-18 12:39:51 +0300

    Tast 6_4_14

--- src/stepik/task_6_4_14/Task_6_4_14.java
+++ src/stepik/task_6_4_14/Task_6_4_14.java
@@ -1,4 +1,167 @@
 package stepik.task_6_4_14;
 
+import java.sql.SQLOutput;
+import java.util.*;
+import java.util.function.Consumer;
+
 public class Task_6_4_14 {
+    public static void main(String[] args) {
+        // Random variables
+        String randomFrom = "1"; // Некоторая случайная строка. Можете выбрать ее самостоятельно.
+        String randomTo = "5";  // Некоторая случайная строка. Можете выбрать ее самостоятельно.
+        int randomSalary = 100;  // Некоторое случайное целое положительное число. Можете выбрать его самостоятельно.
+
+        // Создание списка из трех почтовых сообщений.
+        MailMessage firstMessage = new MailMessage(
+                "Robert Howard",
+                "H.P. Lovecraft",
+                "This \"The Shadow over Innsmouth\" story is real masterpiece, Howard!"
+        );
+
+        assert firstMessage.getFrom().equals("Robert Howard"): "Wrong firstMessage from address";
+        assert firstMessage.getTo().equals("H.P. Lovecraft"): "Wrong firstMessage to address";
+        assert firstMessage.getContent().endsWith("Howard!"): "Wrong firstMessage content ending";
+
+        MailMessage secondMessage = new MailMessage(
+                "Jonathan Nolan",
+                "Christopher Nolan",
+                "Брат, почему все так хвалят только тебя, когда практически все сценарии написал я. Так не честно!"
+        );
+
+        MailMessage thirdMessage = new MailMessage(
+                "Stephen Hawking",
+                "Christopher Nolan",
+                "Я так и не понял Интерстеллар."
+        );
+
+        List<MailMessage> messages = Arrays.asList(
+                firstMessage, secondMessage, thirdMessage
+        );
+
+        // Создание почтового сервиса.
+        MailService<String> mailService = new MailService<>();
+
+        // Обработка списка писем почтовым сервисом
+        messages.stream().forEachOrdered(mailService);
+
+        // Получение и проверка словаря "почтового ящика",
+        // где по получателю можно получить список сообщений, которые были ему отправлены
+        Map<String, List<String>> mailBox = mailService.getMailBox();
+
+//        for (String key : mailBox.keySet()){
+//            System.out.println("key: " + key);
+//            for (String message : mailBox.get(key)) {
+//                System.out.println(message);
+//            }
+//        }
+
+        assert mailBox.get("H.P. Lovecraft").equals(
+                Arrays.asList(
+                        "This \"The Shadow over Innsmouth\" story is real masterpiece, Howard!"
+                )
+        ): "wrong mailService mailbox content (1)";
+
+        assert mailBox.get("Christopher Nolan").equals(
+                Arrays.asList(
+                        "Брат, почему все так хвалят только тебя, когда практически все сценарии написал я. Так не честно!",
+                        "Я так и не понял Интерстеллар."
+                )
+        ): "wrong mailService mailbox content (2)";
+
+        assert mailBox.get(randomTo).equals(Collections.<String>emptyList()): "wrong mailService mailbox content (3)";
+
+        // Создание списка из трех зарплат.
+        Salary salary1 = new Salary("Facebook", "Mark Zuckerberg", 1);
+        Salary salary2 = new Salary("FC Barcelona", "Lionel Messi", Integer.MAX_VALUE);
+        Salary salary3 = new Salary(randomFrom, randomTo, randomSalary);
+
+        // Создание почтового сервиса, обрабатывающего зарплаты.
+        MailService<Integer> salaryService = new MailService<>();
+
+        // Обработка списка зарплат почтовым сервисом
+        Arrays.asList(salary1, salary2, salary3).forEach(salaryService);
+
+        // Получение и проверка словаря "почтового ящика",
+        // где по получателю можно получить список зарплат, которые были ему отправлены.
+        Map<String, List<Integer>> salaries = salaryService.getMailBox();
+        assert salaries.get(salary1.getTo()).equals(Arrays.asList(1)): "wrong salaries mailbox content (1)";
+        assert salaries.get(salary2.getTo()).equals(Arrays.asList(Integer.MAX_VALUE)): "wrong salaries mailbox content (2)";
+        assert salaries.get(randomTo).equals(Arrays.asList(randomSalary)): "wrong salaries mailbox content (3)";
+    }
+}
+
+interface Sendable<T> {
+    String getTo();
+    String getFrom();
+    T getContent();
+}
+
+class UniversalSendable<T> implements Sendable<T> {
+    private String from;
+    private String to;
+    private T content;
+
+    public UniversalSendable(String from, String to, T content) {
+        this.from = from;
+        this.to = to;
+        this.content = content;
+    }
+
+    @Override
+    public String getFrom() {
+        return from;
+    }
+
+    @Override
+    public String getTo() {
+        return to;
+    }
+
+    @Override
+    public T getContent() {
+        return content;
+    }
+}
+
+class MailMessage extends UniversalSendable<String> {
+    public MailMessage(String from, String to, String content) {
+        super(from, to, content);
+    }
+}
+
+class Salary extends UniversalSendable<Integer> {
+    public Salary(String from, String to, Integer content) {
+        super(from, to, content);
+    }
+}
+
+class MailService<T> implements Consumer<Sendable<T>> {
+    Map<String, List<T>> mailBox;
+
+    public MailService() {
+        mailBox = new HashMap<String, List<T>>() {
+            @Override
+            public List<T> get(Object key) {
+                return super.getOrDefault(key, new LinkedList<T>());
+            }
+        };
+    }
+
+    @Override
+    public void accept(Sendable<T> mailMessage) {
+        List<T> list;
+
+        if (mailBox.containsKey(mailMessage.getTo())) {
+            list = mailBox.get(mailMessage.getTo());
+        } else {
+            list = new ArrayList<>();
+        }
+
+        list.add(mailMessage.getContent());
+        mailBox.put(mailMessage.getTo(), list);
+    }
+
+    public Map<String, List<T>> getMailBox() {
+        return mailBox;
+    }
 }
